acfc1486c36ff57f15f6bf88090ae5144d0267e2
Sheikh Ali Raza <aliraza147.ar@gmail.com>
2020-08-15 04:49:26 +0500

    inital repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
+import Parent from './Parent.js';
 function App() {
+
+  let [number, setnumber] = useState(24);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello World from App Component</p>
+      <Parent num = {number}></Parent>
+      <button onClick= { () => setnumber(++number)}>Update Number</button>
     </div>
   );
 }
---
+++ src/Child.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+function Child(props) {
+  return (
+    <div>
+      Child Number {props.num}
+
+    </div>
+  );
+}
+
+export default Child;
